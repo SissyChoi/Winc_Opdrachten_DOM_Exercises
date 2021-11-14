a7fef538072fa084e131097afc32f63ec876f00c
SissyChoi <90140566+SissyChoi@users.noreply.github.com>
2021-11-14 14:53:56 +0100

    Create Opdr_023_Select_Element_with_Chrome_Dev_Tool_Script.js

---
+++ Opdr_023_Select_Element_with_Chrome_Dev_Tool_Script.js
@@ -0,0 +1,5 @@
+const paragraphElement = document.getElementById("first-section");
+console.log(paragraphElement);
+
+// const paragraphElements = document.getElementsByClassName("paragraph");
+// console.log(paragraphElements);
